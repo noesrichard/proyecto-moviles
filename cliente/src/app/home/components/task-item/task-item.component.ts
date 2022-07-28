dfe941bcfa873909b393857d3afc1e9f2a54e0b5
Richard <rcarrion3406@gmail.com>
2022-07-28 13:19:31 -0500

    a;sldkfj

--- cliente/src/app/home/components/task-item/task-item.component.ts
+++ cliente/src/app/home/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {Task, TaskService} from 'src/app/services/task/task.service';
+import { AlertController } from '@ionic/angular';
+import { Task, TaskService } from 'src/app/services/task/task.service';
 
 @Component({
   selector: 'app-task-item',
@@ -7,44 +8,62 @@ import {Task, TaskService} from 'src/app/services/task/task.service';
   styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent implements OnInit {
-
   @Input() task: Task;
 
-  private priorityColor:string = "danger"; 
-  private categoryColor: string = "success"; 
+  private priorityColor: string = 'danger';
+  private categoryColor: string = 'success';
 
-  @Output() onDelete: EventEmitter<any> = new EventEmitter(); 
+  @Output() onDelete: EventEmitter<any> = new EventEmitter();
 
-  constructor(private service: TaskService) { }
+  constructor(private service: TaskService, private alert: AlertController) {}
 
   ngOnInit() {
     this.priorityColor = this.getPriorityColor(this.task);
-    this.categoryColor = this.getCategoryColor(this.task); 
+    this.categoryColor = this.getCategoryColor(this.task);
   }
 
-  delete(){
-    this.service.del(this.task.id).subscribe( res => {
-      console.log(res)
-    } );
-    this.onDelete.emit(true); 
+  delete() {
+    this.alert.create({
+      header: 'Confirme la eliminacion',
+      message: '¿Esta seguro de eliminar la tarea?',
+      buttons: [
+        {
+          text: 'Sí',
+          handler: () => {
+            this.service.del(this.task.id).subscribe((res) => {
+              console.log(res);
+            });
+            this.onDelete.emit(true);
+          },
+        },
+        'Cancelar',
+      ],
+    }).then(alert => alert.present());
   }
 
-  getPriorityColor(task: Task){ 
-    switch(task.priority){ 
-      case "Importante": return "warning"; 
-      case "Poco Importante": return "primary";
-      default: return "danger"; 
+  getPriorityColor(task: Task) {
+    switch (task.priority) {
+      case 'Importante':
+        return 'warning';
+      case 'Poco Importante':
+        return 'primary';
+      default:
+        return 'danger';
     }
   }
 
-  getCategoryColor(task: Task){ 
-    switch(task.category){ 
-      case "Hogar": return "warning"; 
-      case "Trabajo": return "primary"; 
-      case "Otros": return "secondary"; 
-      case "Educacion": return "danger"; 
-      default: return "warning"; 
+  getCategoryColor(task: Task) {
+    switch (task.category) {
+      case 'Hogar':
+        return 'warning';
+      case 'Trabajo':
+        return 'primary';
+      case 'Otros':
+        return 'secondary';
+      case 'Educacion':
+        return 'danger';
+      default:
+        return 'warning';
     }
   }
-
 }
